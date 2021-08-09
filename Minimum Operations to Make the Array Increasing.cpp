ce7372235d932d1b466f49ef4f7375eab452b363
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-08-09 19:57:11 +0530

    cnt+=(nums[i]+1-nums[i+1]); nums[i+1]=nums[i]+1;

---
+++ Minimum Operations to Make the Array Increasing.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n=nums.size(), cnt=0;
+        for(int i=0; i<n-1; i++)
+        {
+            if(nums[i]>=nums[i+1])
+            {
+                cnt+=(nums[i]+1-nums[i+1]);
+                nums[i+1]=nums[i]+1;
+            }
+        }
+        return cnt;
+        
+        
+    }
+};
