2f994381cc3c1b08e62073589fd2d428503e97aa
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-08-30 20:41:17 +0530

    Iterative->Stack

---
+++ Tree/Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,21 @@
+lass Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>v;
+        if(root==NULL)
+            return v;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(st.empty()==false)
+        {
+            root=st.top();
+            st.pop();
+            v.push_back(root->val);
+            if(root->right!=NULL)
+                st.push(root->right);
+            if(root->left!=NULL)
+                st.push(root->left);
+        }
+        return v;
+    }
+};
